73de35a2e41693bc340dbb6f3a4d347b119c85f7
Mohammed Karimi <mohammed.karimi01@hotmail.com>
2021-09-24 12:04:49 +0200

    Labaration 1 - Mohammed Karimi

--- Laboration 1 Mohammed Karimi/Program.cs
+++ Laboration 1 Mohammed Karimi/Program.cs
@@ -7,28 +7,61 @@ namespace Laboration_1_Mohammed_Karimi
     {
         static void Main(string[] args)
         {
-            string MainString = "29535123p48723487597645723645"; //Här har jag deklarerat min hårdkodade sträng "MainString"
+            // Gjort av Mohammed Karimi - Labaration 1 
+            // 2021-09-22
+
+            string MainString = "29535123p48723487597645723645";
+            string substring = string.Empty;                //Denna sträng sparars alla hittade tal.
+            string FirstSubstringMatch = String.Empty;      //Denna sträng sparar de hittade talen innan den har hittat ett mönster.
+            string SecondSubstringMatch = string.Empty;     //Denna sträng sparar de talen den hittar efter att den har hittat ett mönster.
+            Decimal TotalAmount = 0;
 
             for (int i = 0; i < MainString.Length; i++)
             {
-                if (Char.IsDigit(MainString[i])) // Denna del kollar om Strängen "MainString" har en siffra i sig
+                int temp = 0;
+                bool isNumber = int.TryParse(MainString[i] + "", out temp);
+                if (isNumber == false)                      //Denna loop kollar om IsNumber Är ett nummer eller ej, om det är ett tal fortsätter programmet.
                 {
-                    for (int L = i + 1; L < MainString.Length; L++)
+                    continue;
+                }
+
+                for (int j = i + 1; j < MainString.Length; j++)
+                {
+
+                    if (!char.IsDigit(MainString[j]))       //Denna Loop kontrollerar om angivet tal i 'MainString' är inte en siffra, om det inte är det slutar loopen.
                     {
-                        if (MainString[i] == MainString[L]) // Denna del kollar om den första hittade siffran matchar den andra hittade siffran. Och sedan skriver ut samt färgar de matchande delarna.
-                        {
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.WriteLine(MainString.Substring(i, L - i + 1));
-                            break;
-                        }
-                        else if (!Char.IsDigit(MainString[L]))// Denna del kollar om MainString[L] inte är en siffra, om den inte är det hoppar den tillbaka i loopen och fortsätter.
-                        {
-                            break;
-                        }
+                        break;
+                    }
+                    if (MainString[i] == MainString[j])
+                    {
+                        substring = MainString.Substring(i, j - i + 1);//Denna Sträng sparar mönster som den hittat i substringen.
+
+                        Decimal result;
+                        Decimal.TryParse(substring, out result);
+                        TotalAmount += result;  //Denna bit adderar de två olika talen och räknar ut summan på dem.
+
+                        FirstSubstringMatch = MainString.Substring(0, MainString.IndexOf(substring)); //Denna del kollar alla de första siffrorna innan p:et, och sparar de hittade mönsterna och skriver ut den.
+                        SecondSubstringMatch = MainString.Substring(substring.Length + FirstSubstringMatch.Length, (MainString.Length) - (substring.Length + FirstSubstringMatch.Length));
+                        // Denna del åvan kollar de sista siffrorna efter p:et och sparar mönsterna samt skriver ner den.
+
+                        //Denna bit färgar de olika delarna i strängen, och skriver ut dem.
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write(FirstSubstringMatch);
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(substring);
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine(SecondSubstringMatch);
+                        break;
                     }
                 }
             }
-            Console.ReadKey();
+            //Denna bit skriver ut den totala summan av de två talen samt färgar dem lite snyggt.
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Den totala summan är: ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(TotalAmount + "\n");
+            Console.ResetColor();
         }
     }
 }
